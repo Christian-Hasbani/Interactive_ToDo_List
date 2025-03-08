66a53103f7b17d90a5459df5db345842f9f449c8
Christian Hasbani <christian@archlinux.localdomain>
2025-03-08 11:21:13 +0100

    Initial commit front

---
+++ Frontend/angular-todolist/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = signal('angular-todolist');
+  test : boolean = false;
+
+}
